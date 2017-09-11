c667d0dbf697f59d6d30696b9ab4769f1014f21b
Mercutio217 <l.bielenin@gmail.com>
2017-09-11 10:16:05 +0200

    Refactor of Convert method in Movie Controller.

---
+++ PlanUp/Controllers/MovieController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using PlanUp.Models;
+using TMDbLib.Client;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Search;
+
+namespace PlanUp.Controllers
+{
+    public class MovieController : Controller
+    {
+        public static TMDbClient Client = new TMDbClient("e7a445aaa97ddc684c2404b990fb7087");
+        public static List<SearchMovie> MovieList = Client.GetMovieTopRatedListAsync().Result.Results;
+
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var model = Converter();
+
+            return View(model);
+        }
+
+        internal static Movie[] Converter()
+        {
+            Movie[] movieArray = new Movie[3];
+            var random = new Random();
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                var slotMovie = MovieList[random.Next(MovieList.Count)];
+                Movie movie;
+                try
+                {
+                    var release = (DateTime) slotMovie.ReleaseDate;
+                    movie = new Movie(slotMovie.Title, release.ToString("MMMM dd, yyyy"), slotMovie.PosterPath);
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                    movie = new Movie(slotMovie.Title, null, slotMovie.PosterPath);
+                }
+                movieArray[i] = movie;
+            }
+
+            return movieArray;
+        }
+    }
+}
