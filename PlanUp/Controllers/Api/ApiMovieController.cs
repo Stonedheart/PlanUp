15c16a91e5e6ddd7337e2a63827deee4b1f43c2e
Mercutio217 <l.bielenin@gmail.com>
2017-09-14 14:58:15 +0200

    Reformat of ApiMovieController

--- PlanUp/Controllers/Api/ApiMovieController.cs
+++ PlanUp/Controllers/Api/ApiMovieController.cs
@@ -12,96 +12,48 @@ namespace PlanUp.Controllers.Api
 {
     public class ApiMovieController : IApi
     {
-        internal string ApiKey {get;set;}
-        internal TMDbClient Client;
-        internal DatabaseMovie TmdbMovie;
-        internal Movie Proposition { get; set; }
+        private TMDbClient _client;
+        private DatabaseMovie _tmdbMovie;
 
-        public ApiMovieController(string apiKey="e7a445aaa97ddc684c2404b990fb7087")
+        public ApiMovieController(string apiKey = "e7a445aaa97ddc684c2404b990fb7087")
         {
-            ApiKey = apiKey;
             SetConnection(apiKey);
         }
 
-        public void  SetConnection(string newKey)
+        public void SetConnection(string newKey)
         {
-            Client = new TMDbClient(newKey);
+            _client = new TMDbClient(newKey);
         }
 
-        public void Convert()
-        {
-            throw new NotImplementedException();
-        }
-
-        public object GetPropostition()
-        {
-            SetDefaultProposition();
-            return Proposition;
-        }
-
-        public int GetRandomNumber()
+        private int GetRandomNumber()
         {
             var random = new Random(99999);
 
             return random.Next();
-
         }
 
-        public DatabaseMovie GetDatabaseMovie(int? id)
+        internal DatabaseMovie GetDatabaseMovie(int? id)
         {
             var movieId = id ?? GetRandomNumber();
             if (id > 99999)
             {
                 throw new InvalidOperationException("Id should not be greater than 99999!");
             }
-            var result = Client.GetMovieAsync(movieId).Result;
+            var result = _client.GetMovieAsync(movieId).Result;
 
             return result;
         }
 
-        public void SetDatabaseMovie()
+        internal void SetDatabaseMovie()
         {
             var movie = GetDatabaseMovie(GetRandomNumber());
-            TmdbMovie = movie;
+            _tmdbMovie = movie;
         }
 
-        public Movie Convert(DatabaseMovie tmdbMovie)
+        internal DatabaseMovie GetNextMovie()
         {
-            var resultMovie = new Movie(tmdbMovie.Title, tmdbMovie.ReleaseDate.ToString(), tmdbMovie.PosterPath);
-
-            return resultMovie;
+            SetDatabaseMovie();
+            return _tmdbMovie;
         }
-
-        public void SetDefaultProposition()
-        {
-            Proposition = Convert(GetDatabaseMovie(GetRandomNumber()));
-        }
-
-
-//        internal static Movie[] Converter()
-//        {
-//            var movieArray = new Movie[3];
-//            var random = new Random();
-//
-//
-//            for (var i = 0; i < 3; i++)
-//            {
-//                var slotMovie = MovieList[random.Next(MovieList.Count)];
-//                Movie movie;
-//                try
-//                {
-//                    var release = (DateTime) slotMovie.ReleaseDate;
-//                    movie = new Movie(slotMovie.Title, release.ToString("MMMM dd, yyyy"), slotMovie.PosterPath);
-//                }
-//                catch (InvalidOperationException e)
-//                {
-//                    Console.WriteLine(e.Message);
-//                    movie = new Movie(slotMovie.Title, null, slotMovie.PosterPath);
-//                }
-//                movieArray[i] = movie;
-//            }
-//
-//            return movieArray;
-//        }
     }
 }
--- PlanUp/Controllers/Api/IApi.cs
+++ PlanUp/Controllers/Api/IApi.cs
@@ -3,10 +3,7 @@
 namespace PlanUp.Controllers.Api
 {
     public interface IApi
-    {
-        void  SetConnection(string newKey);
-        void Convert();
-        object GetNextPropostition();
-        // git comment
+{
+    
     }
 }
