ff5d4ac403ab0147c2a73697cdc981ca7288d1b4
Mercutio217 <l.bielenin@gmail.com>
2017-09-14 15:15:50 +0200

    Implement class MovieConerter.

--- PlanUp/Converters/MovieConverter.cs
+++ PlanUp/Converters/MovieConverter.cs
@@ -1,9 +1,26 @@
-﻿using DatabaseMovie = TMDbLib.Objects.Movies;
+﻿using System;
+using PlanUp.Models;
+using DatabaseMovie = TMDbLib.Objects.Movies.Movie;
 
 namespace PlanUp.Converters
 {
     public class MovieConverter
     {
-        
+        public Movie Convert(DatabaseMovie movieToConvert)
+        {
+            if (movieToConvert.Runtime != null)
+            {
+                var resultMovie =
+                    new Movie(movieToConvert.Title,
+                        movieToConvert.ReleaseDate.ToString(),
+                        movieToConvert.PosterPath, (int) movieToConvert.Runtime);
+
+                return resultMovie;
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
     }
 }
--- PlanUp/Models/Movie.cs
+++ PlanUp/Models/Movie.cs
@@ -9,10 +9,11 @@
         public string PosterPath { get; }
         public int Duration { get; }
 
-        public Movie(string title, string year, string posterPath)
+        public Movie(string title, string year, string posterPath, int duration)
         {
             Title = title;
             Year = year;
+            Duration = duration;
             PosterPath = "https://image.tmdb.org/t/p/w320" + posterPath;
         }
 
