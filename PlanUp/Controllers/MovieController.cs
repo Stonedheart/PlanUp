3e203067f0577e8fe4247a6cf09df986a2650f26
Mercutio217 <l.bielenin@gmail.com>
2017-09-14 16:11:26 +0200

    Implement converter to movie controller.

--- PlanUp/Controllers/MovieController.cs
+++ PlanUp/Controllers/MovieController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using PlanUp.Controllers.Api;
+using PlanUp.Converters;
 using PlanUp.Models;
 using TMDbLib.Client;
 using TMDbLib.Objects.General;
@@ -13,6 +15,23 @@ namespace PlanUp.Controllers
 {
     public class MovieController : Controller
     {
+        private ApiMovieController _movieController = new ApiMovieController();
+        private MovieConverter _movieConverter;
+
+        public ActionResult Index()
+        {
+            var movieArray = new Movie[3];
+                
+            for (var i = 0; i < movieArray.Length; i++)
+            {
+                var databaseMovie = _movieController.GetNextMovie();
+                _movieConverter = new MovieConverter(databaseMovie);
+                movieArray[i] = _movieConverter.Convert();
+
+            }
+            
+            return View(movieArray);
+        }
        
     }
 }
