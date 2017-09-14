d8a48481145471b1497e2141c44cdccc3c72c36f
Klaudia <canimi18@gmail.com>
2017-09-14 14:15:34 +0200

    Createnew class

---
+++ PlanUp/Models/HomeEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlanUp.Models
+{
+    public class HomeEvent
+    {
+        public int Id { get; set; }
+        public string HomeEventName { get; set; }
+        public EventCategory EventCategory { get; set; }
+        public int Duration { get; set; }
+
+        public HomeEvent(string name, EventCategory eventCategory, int duration)
+        {
+            this.HomeEventName = name;
+            this.EventCategory = eventCategory;
+            this.Duration = duration;
+        }
+
+    }
+}
