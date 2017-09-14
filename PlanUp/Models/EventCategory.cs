aaabb87da9697f28c0e2c683b836ce52b6758d77
Klaudia <canimi18@gmail.com>
2017-09-14 14:15:09 +0200

    Createnew class

---
+++ PlanUp/Models/EventCategory.cs
@@ -0,0 +1,13 @@
+﻿namespace PlanUp.Models
+{
+    internal class EventCategory
+    {
+        private int Id { get; set; }
+        private string CategoryName { get; set; }
+
+        public EventCategory(string categoryName)
+        {
+            this.CategoryName = categoryName;
+        }
+    }
+}
