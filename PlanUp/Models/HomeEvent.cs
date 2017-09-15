772cec72d7b5b6a5ac0c954ad7c80ee3370d0bdd
Klaudia <canimi18@gmail.com>
2017-09-15 11:26:55 +0200

    Testing change obj to int in model

--- PlanUp/Models/HomeEvent.cs
+++ PlanUp/Models/HomeEvent.cs
@@ -9,10 +9,15 @@ namespace PlanUp.Models
     {
         public int Id { get; set; }
         public string HomeEventName { get; set; }
-        public EventCategory EventCategory { get; set; }
+        public int EventCategory { get; set; }
         public int Duration { get; set; }
 
-        public HomeEvent(string name, EventCategory eventCategory, int duration)
+        public HomeEvent()
+        {
+    
+        }
+
+        public HomeEvent(string name, int eventCategory, int duration)
         {
             this.HomeEventName = name;
             this.EventCategory = eventCategory;
