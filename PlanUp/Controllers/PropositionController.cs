71d6e00bf0060d937fa6b6973999be7e22909455
Mercutio217 <l.bielenin@gmail.com>
2017-09-22 15:32:07 +0200

    Implement PropostionsController

---
+++ PlanUp/Controllers/PropositionController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using PlanUp.Models;
+using TMDbLib.Objects.Lists;
+
+namespace PlanUp.Controllers
+{
+    public class PropositionController
+    {
+        public AbstractHomeEvent FirstProposition;
+        public AbstractHomeEvent SecondPropostion;
+        public AbstractHomeEvent ThirdProposition;
+        public List<AbstractHomeEvent> listOfPropositions;
+    }
+}
