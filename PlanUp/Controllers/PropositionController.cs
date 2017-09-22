da050edc162009c9c925451f3ecc275b0a36226b
Mercutio217 <l.bielenin@gmail.com>
2017-09-22 15:49:58 +0200

    IMplement setAll funtion to PropostioinController

--- PlanUp/Controllers/PropositionController.cs
+++ PlanUp/Controllers/PropositionController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using PlanUp.Controllers.Api;
+using PlanUp.Converters;
 using PlanUp.Models;
 using TMDbLib.Objects.Lists;
 
@@ -6,9 +8,27 @@ namespace PlanUp.Controllers
 {
     public class PropositionController
     {
-        public AbstractHomeEvent FirstProposition;
-        public AbstractHomeEvent SecondPropostion;
-        public AbstractHomeEvent ThirdProposition;
-        public List<AbstractHomeEvent> listOfPropositions;
+        private ApiMovieController apiController;
+        private MovieConverter _converter;
+        public AbstractHomeEvent FirstProposition { get; set; }
+        public AbstractHomeEvent SecondPropostion { get; set; }
+        public AbstractHomeEvent ThirdProposition { get; set; }
+        public List<AbstractHomeEvent> listOfPropositions { get; set; }
+
+        public PropositionController()
+        {
+            apiController = new ApiMovieController();
+            listOfPropositions = 
+                new List<AbstractHomeEvent>() {FirstProposition, SecondPropostion, ThirdProposition};
+        }
+
+        void SetAll()
+        {
+            for (int i = 0; i < listOfPropositions.Count; i++)
+            {
+                _converter = new MovieConverter(apiController.GetNextMovie());
+                listOfPropositions[i] = _converter.Convert();
+            }
+        }
     }
 }
