02a1f12dc23b5b13aa4e3876b53bf50c0233e636
Mercutio217 <l.bielenin@gmail.com>
2017-09-22 15:13:21 +0200

    Remodell Movie model.

--- PlanUp/Models/Movie.cs
+++ PlanUp/Models/Movie.cs
@@ -1,19 +1,19 @@
 ﻿namespace PlanUp.Models
 {
-    public class Movie
+    public class Movie : AbstractHomeEvent
     {
-        public string Title { get; }
         public string Director { get; set; }
         public string Genre { get; set; }
         public string Year { get; set; }
         public string PosterPath { get; }
         public int Duration { get; set; }
 
-        public Movie(string title, string year, string posterPath)
+        public Movie(string title, string year, string posterPath, int duration)
         {
             Title = title;
             Year = year;
             PosterPath = "https://image.tmdb.org/t/p/w320" + posterPath;
+            Duration = duration;
         }
     }
 }
