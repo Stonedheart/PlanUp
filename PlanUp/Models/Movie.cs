b95db4b25200747a343c21e1a7d03a52e5cd9db4
Mercutio217 <l.bielenin@gmail.com>
2017-09-22 15:19:39 +0200

    Remodell MovieCOntroller regarding remodelled Movie model.

--- PlanUp/Models/Movie.cs
+++ PlanUp/Models/Movie.cs
@@ -5,15 +5,12 @@
         public string Director { get; set; }
         public string Genre { get; set; }
         public string Year { get; set; }
-        public string PosterPath { get; }
-        public int Duration { get; set; }
 
-        public Movie(string title, string year, string posterPath, int duration)
+        public Movie(string title, string year, string imagePath)
         {
             Title = title;
             Year = year;
-            PosterPath = "https://image.tmdb.org/t/p/w320" + posterPath;
-            Duration = duration;
+            ImagePath = "https://image.tmdb.org/t/p/w320" + imagePath;
         }
     }
 }
