39880087beada89d74a69be305f4ed1ab335caac
Stonedheart <tytomaszto@gmail.com>
2017-10-08 14:56:50 +0200

    Add constructor and GetEtag method to abstract class

--- PlanUp/Models/AbstractYouTubeVideoProposition.cs
+++ PlanUp/Models/AbstractYouTubeVideoProposition.cs
@@ -11,5 +11,16 @@ namespace PlanUp.Models
         public string Etag { get; }
         public string Description { get; }
 
+        public AbstractYouTubeVideoProposition(string title, string Id, string description)
+        {
+            Title = title;
+            Etag = GetEtag(Id);
+            Description = description;
+        }
+
+        private string GetEtag(string etag)
+        {
+            return "https://www.youtube.com/embed/" + etag + "?controls=0";
+        }
     }
 }
