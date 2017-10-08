d9ef1b34075ef49f069cff3ba5ac82a46c863e49
Stonedheart <tytomaszto@gmail.com>
2017-10-08 14:48:39 +0200

    Add method GetPropositionFromYouTube

--- PlanUp/Controllers/YoutubeApiController.cs
+++ PlanUp/Controllers/YoutubeApiController.cs
@@ -3,22 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using Google.Apis.Services;
 using Google.Apis.YouTube.v3;
+using TMDbLib.Objects.General;
 
 namespace PlanUp.Controllers
 {
     public class YoutubeApiController : ApiController
     {
+        private YouTubeService _youTubeService;
+
 
         public void SetConnection(string apiKey = "AIzaSyDkMyIyaZSKaBdaKhbdCt1YSPxGG2ewoII")
         {
-            YouTubeService youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            _youTubeService = new YouTubeService(new BaseClientService.Initializer()
             {
                 ApiKey = apiKey,
                 ApplicationName = this.GetType().ToString()
             });
+        }
+
+
+        public async Task<YouTubeProposition[]> GetPropositionFromYouTube(string q, int maxResults = 50)
+        {
+            var searchListRequest = _youTubeService.Search.List("snippet");
+            searchListRequest.Q = q;
+            searchListRequest.MaxResults = maxResults;
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+            return _youTubeSearchResultConverter.Convert(searchListResponse);
         }
     }
 }
