3f460debcfc2d7aed76a1993755ac8322070eccb
Stonedheart <tytomaszto@gmail.com>
2017-10-08 14:57:20 +0200

    Remove unnecessary usings

--- PlanUp/Models/AbstractYouTubeVideoProposition.cs
+++ PlanUp/Models/AbstractYouTubeVideoProposition.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace PlanUp.Models
+﻿namespace PlanUp.Models
 {
     public class AbstractYouTubeVideoProposition
     {
