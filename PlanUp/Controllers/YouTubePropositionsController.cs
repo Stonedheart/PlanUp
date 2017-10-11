02a815aa4371f246855298a1bb5d043981da663a
Stonedheart <tytomaszto@gmail.com>
2017-10-11 16:16:36 +0200

    Add controller for propositions from youtube

---
+++ PlanUp/Controllers/YouTubePropositionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlanUp.Controllers
+{
+    public class YouTubePropositionsController : Controller
+    {
+        // GET: YouTubePropositions
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
