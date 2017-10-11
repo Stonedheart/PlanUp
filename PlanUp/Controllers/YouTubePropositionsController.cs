2b62070fc0f89cbbe3860b54e12499e53c6bc828
Stonedheart <tytomaszto@gmail.com>
2017-10-11 16:28:55 +0200

    Add constructor with fields

--- PlanUp/Controllers/YouTubePropositionsController.cs
+++ PlanUp/Controllers/YouTubePropositionsController.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using PlanUp.Converters;
+using PlanUp.Factories;
+using PlanUp.Models;
+using PlanUp.Models.YouTubePropositions;
 
 namespace PlanUp.Controllers
 {
     public class YouTubePropositionsController : Controller
     {
-        // GET: YouTubePropositions
-        public ActionResult Index()
+        private YouTubePropositionsFactory _factory;
+        private YouTubeSearchResultConverter _converter;
+        private YoutubeApiController _apiController;
+
+        public YouTubePropositionsController()
         {
-            return View();
+            _factory = new YouTubePropositionsFactory();
+            _converter = new YouTubeSearchResultConverter(_factory);
+            _apiController = new YoutubeApiController(_converter);
         }
     }
 }
