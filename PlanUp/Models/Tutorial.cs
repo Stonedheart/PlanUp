dddafb6410a0a649ee6e2de11ca5e4bbbf1be057
Stonedheart <tytomaszto@gmail.com>
2017-10-11 14:07:45 +0200

    Add Tutorial class

---
+++ PlanUp/Models/Tutorial.cs
@@ -0,0 +1,9 @@
+﻿namespace PlanUp.Models
+{
+    public class Tutorial : AbstractYouTubeVideoProposition
+    {
+        public Tutorial(string title, string etag, string description) : base(title, etag, description)
+        {
+        }
+    }
+}
