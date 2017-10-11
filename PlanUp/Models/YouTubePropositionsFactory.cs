39025593a391b0e0f5ea9192841c833ad678fefc
Stonedheart <tytomaszto@gmail.com>
2017-10-11 14:08:09 +0200

    Add Factory for Youtube propositions

---
+++ PlanUp/Models/YouTubePropositionsFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Google.Apis.YouTube.v3.Data;
+
+namespace PlanUp.Models
+{
+    public class YouTubePropositionsFactory
+    {
+        public SearchResult SearchResult { get; set; }
+
+        public AbstractYouTubeVideoProposition Create(YoutubePropositionType type)
+        {
+            switch (type)
+            {
+                case YoutubePropositionType.Tutorial:
+                    return new Tutorial(SearchResult.Snippet.Title, SearchResult.Id.VideoId, SearchResult.Snippet.Description);
+            }
+            return null;
+        }
+    }
+}
