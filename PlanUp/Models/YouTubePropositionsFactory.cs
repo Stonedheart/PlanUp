7bf973dd37026f0d273328450492f68637d52758
Stonedheart <tytomaszto@gmail.com>
2017-10-11 15:52:07 +0200

    Add cases for different propositions to factory

--- PlanUp/Models/YouTubePropositionsFactory.cs
+++ PlanUp/Models/YouTubePropositionsFactory.cs
@@ -4,12 +4,27 @@ namespace PlanUp.Models
 {
     public class YouTubePropositionsFactory
     {
-        public SearchResult SearchResult { get; set; }
+        public SearchResult SearchResult { get; set; } 
 
         public AbstractYouTubeVideoProposition Create(YoutubePropositionType type)
         {
             switch (type)
             {
+                case YoutubePropositionType.Aerobic:
+                    return new Aerobic(SearchResult.Snippet.Title, SearchResult.Id.VideoId, SearchResult.Snippet.Description);
+
+                case YoutubePropositionType.Audiobook:
+                    return new Audiobook(SearchResult.Snippet.Title, SearchResult.Id.VideoId, SearchResult.Snippet.Description);
+
+                case YoutubePropositionType.Cardio:
+                    return new Cardio(SearchResult.Snippet.Title, SearchResult.Id.VideoId, SearchResult.Snippet.Description);
+
+                case YoutubePropositionType.Concert:
+                    return new Concert(SearchResult.Snippet.Title, SearchResult.Id.VideoId, SearchResult.Snippet.Description);
+
+                case YoutubePropositionType.Festival:
+                    return new Festival(SearchResult.Snippet.Title, SearchResult.Id.VideoId, SearchResult.Snippet.Description);
+
                 case YoutubePropositionType.Tutorial:
                     return new Tutorial(SearchResult.Snippet.Title, SearchResult.Id.VideoId, SearchResult.Snippet.Description);
             }
