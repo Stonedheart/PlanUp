1cd88ab9d699a0c74807f6e1f97500234db72820
Stonedheart <tytomaszto@gmail.com>
2017-10-11 14:12:55 +0200

    Add method Index to check if everything works

--- PlanUp/Controllers/YoutubeApiController.cs
+++ PlanUp/Controllers/YoutubeApiController.cs
@@ -5,15 +5,20 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using Google.Apis.Services;
 using Google.Apis.YouTube.v3;
+using PlanUp.Converters;
+using PlanUp.Models;
 using TMDbLib.Objects.General;
 
 namespace PlanUp.Controllers
 {
-    public class YoutubeApiController : ApiController
+    public class YoutubeApiController : Controller
     {
         private YouTubeService _youTubeService;
+        private readonly YouTubeSearchResultConverter _youTubeSearchResultConverter = new YouTubeSearchResultConverter();
 
 
         public void SetConnection(string apiKey = "AIzaSyDkMyIyaZSKaBdaKhbdCt1YSPxGG2ewoII")
@@ -26,13 +31,35 @@ namespace PlanUp.Controllers
         }
 
 
-        public async Task<YouTubeProposition[]> GetPropositionFromYouTube(string q, int maxResults = 50)
+        public async Task<AbstractYouTubeVideoProposition[]> GetPropositionFromYouTube(string q, int maxResults = 50)
         {
             var searchListRequest = _youTubeService.Search.List("snippet");
             searchListRequest.Q = q;
             searchListRequest.MaxResults = maxResults;
             var searchListResponse = await searchListRequest.ExecuteAsync();
-            return _youTubeSearchResultConverter.Convert(searchListResponse);
+            _youTubeSearchResultConverter.Convert(searchListResponse);
+            return _youTubeSearchResultConverter.propositions;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                SetConnection();
+                var query = YoutubePropositionType.Tutorial;
+                var result = await GetPropositionFromYouTube(query.ToString());
+                var model = new Tutorial[3];
+                for (var i = 0; i < model.Length; i++)
+                {
+                    model[i] = (Tutorial) result[i];
+                }
+                return View(model);
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            return Redirect("Index");
         }
     }
 }
