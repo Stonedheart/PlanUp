64aadcc829a64b913f2a7aea00d00e5038927dae
Stonedheart <tytomaszto@gmail.com>
2017-10-11 14:11:54 +0200

    Add enum with youtube proposition types

---
+++ PlanUp/Models/YoutubePropositionType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlanUp.Models
+{
+    public enum YoutubePropositionType
+    {
+        Aerobic,
+        Audiobook,
+        Cardio,
+        Concert,
+        Festival,
+        Tutorial
+    }
+}
