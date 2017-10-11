b32886ef3ad73c7cedc1c08a19b85df1f4fe6f9a
Stonedheart <tytomaszto@gmail.com>
2017-10-11 16:02:16 +0200

    Move existing propositions classes to proper folder

--- PlanUp/Models/Tutorial.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PlanUp.Models
-{
-    public class Tutorial : AbstractYouTubeVideoProposition
-    {
-        public Tutorial(string title, string etag, string description) : base(title, etag, description)
-        {
-        }
-    }
-}
