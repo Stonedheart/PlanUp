ba1c69a437013f95ce8749679d41c4aedb4b65ef
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-10-21 11:39:24 +0300

    Add JQuery method in order to save rejected music proposition to db.

--- PlanUp/Scripts/JSForMusicView.js
+++ PlanUp/Scripts/JSForMusicView.js
@@ -1,6 +1,5 @@
 ﻿
-    
-    //document.getElementById("description-body").style.display = "none";
+
     function GeneratePropositionInput()
     {
         document.getElementById("genrePreferenceForm").style.display = "block";
@@ -13,3 +12,24 @@
     {
         document.getElementById("description-body").showPopup(item.description);
     }
+
+    $(function () {
+        $(".Next").click(function (e) {
+
+            var defaultValue = 'def';
+            var myData = [{ Title: '@Model[0].Title', Description: defaultValue, Etag: '@Model[0].Etag' },
+            { Title: '@Model[1].Title', Description: defaultValue, Etag: '@Model[1].Etag' },
+            { Title: '@Model[2].Title', Description: defaultValue, Etag: '@Model[2].Etag' }];
+            $.ajax({
+                url: "@Url.Action("SaveRejectedToDatabase", "Music")",
+                type: "POST",
+                dataType: "json",
+                contentType: 'application/json',
+                data: JSON.stringify(myData),
+                success: function () {
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                }
+            });
+        });
+    })
