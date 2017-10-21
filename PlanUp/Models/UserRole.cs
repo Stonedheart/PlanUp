f3bb6fe914ab90772a4d811e1fd77a7da6c7bdec
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-10-21 23:01:45 +0300

    Add UserRole class with 'isLazy

---
+++ PlanUp/Models/UserRole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlanUp.Models
+{
+    public class UserRole
+    {
+        public static bool IsLazy { get; set; }
+    }  
+}
