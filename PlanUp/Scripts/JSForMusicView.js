0cbae05026c0e0e32873ed045c1eb7d30815ce95
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-10-21 11:37:06 +0300

    Add logic to display items to get new music proposition.

---
+++ PlanUp/Scripts/JSForMusicView.js
@@ -0,0 +1,15 @@
+﻿
+    
+    //document.getElementById("description-body").style.display = "none";
+    function GeneratePropositionInput()
+    {
+        document.getElementById("genrePreferenceForm").style.display = "block";
+    }
+    function GenerateFormProposition()
+    {
+        document.getElementById("genreForm").style.display = "block";
+    }
+    function ShowDescription()
+    {
+        document.getElementById("description-body").showPopup(item.description);
+    }
